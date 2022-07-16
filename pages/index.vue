88b7af1156ae044e4d8a0a4753ae321d54f931b1
ADereglazov <bt745412>
2022-07-17 00:08:53 +0300

    init

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1 class="title">Hello!</h1>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
+<style lang="less" scoped>
+  .title {
+    color: tomato;
+  }
+</style>
